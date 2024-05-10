35374cad2b1985c73d59ed46c1ce5b11ce9db907
dariaag <daria@Darias-MacBook-Air.local>
2024-05-10 14:33:24 +0300

    init

--- target/package/json_size-0.1.0/target/debug/deps/json_size-bd9a76552f4cb980.d
+++
@@ -1,7 +0,0 @@
-/Users/daria/Desktop/github/json_size/target/package/json_size-0.1.0/target/debug/deps/libjson_size-bd9a76552f4cb980.rmeta: src/lib.rs
-
-/Users/daria/Desktop/github/json_size/target/package/json_size-0.1.0/target/debug/deps/libjson_size-bd9a76552f4cb980.rlib: src/lib.rs
-
-/Users/daria/Desktop/github/json_size/target/package/json_size-0.1.0/target/debug/deps/json_size-bd9a76552f4cb980.d: src/lib.rs
-
-src/lib.rs:
--- target/package/json_size-0.1.0/target/debug/libjson_size.d
+++
@@ -1,1 +0,0 @@
-/Users/daria/Desktop/github/json_size/target/package/json_size-0.1.0/target/debug/libjson_size.rlib: /Users/daria/Desktop/github/json_size/target/package/json_size-0.1.0/src/lib.rs
